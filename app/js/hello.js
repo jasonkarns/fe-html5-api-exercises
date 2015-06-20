4dc9395b48a04a9a8a60754ff29711c43309b9e3
Jason Karns <jason@karns.name>
2015-06-20 17:17:54 -0400

    implementation

--- app/js/hello.js
+++ app/js/hello.js
@@ -2,8 +2,28 @@ document.getElementById('status').innerText = history.pushState ? 'available' : 
 
 
 document.getElementById('nav').addEventListener('click', function(event){
+  event.preventDefault();
+
+  if(event.target.matches("a")){
+    var url = event.target.href;
+    var title = event.target.innerText;
+
+    var state = { foo: "bar", title: title, url: url };
+
+    history.pushState(state, title, url);
+  }
 });
 
 
 document.getElementById('show-state').addEventListener('click', function(){
+
+  document.getElementById('current-state').innerText = JSON.stringify(history.state);
+
+});
+
+window.addEventListener('popstate', function(e){
+
+  var title = event.state ? event.state.title : "no state object";
+  document.getElementById('history').insertAdjacentHTML('beforeend', "<li>"+ title +"</li>");
+
 });
