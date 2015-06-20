5d20d2a3927e6e4f9585b20b1fe963004e2d4d2f
Jason Karns <jason@karns.name>
2015-06-20 17:17:20 -0400

    skeleton

--- app/js/hello.js
+++ app/js/hello.js
@@ -1,14 +1,9 @@
-window.helloText = function() {
-  return 'Hello, World!';
-};
+document.getElementById('status').innerText = history.pushState ? 'available' : 'unavailable';
 
-window.hello = function() {
-  html = JST['app/templates/hello.us']({text: helloText()});
-  document.body.innerHTML += html;
-};
 
-if(window.addEventListener) {
-  window.addEventListener('DOMContentLoaded', hello, false);
-} else {
-  window.attachEvent('onload', hello);
-}
+document.getElementById('nav').addEventListener('click', function(event){
+});
+
+
+document.getElementById('show-state').addEventListener('click', function(){
+});
