b377d9515e19b3440302b3718293dd40ade0915f
Jason Karns <jason@karns.name>
2015-06-21 11:45:19 -0400

    implementation

--- app/js/hello.js
+++ app/js/hello.js
@@ -1,14 +1,33 @@
-window.helloText = function() {
-  return 'Hello, World!';
+
+var handle = {
+  set: function(storage, form) {
+    var key = form.querySelector("[name=key]").value;
+    var value = form.querySelector("[name=value]").value;
+
+    storage.setItem(key, value);
+    form.reset();
+  },
+  get: function(storage, form) {
+    var key = form.querySelector("[name=key]").value;
+    var value = form.querySelector("[name=value]");
+
+    value.value = storage.getItem(key);
+  },
+  clear: function(storage, form) {
+    storage.clear();
+    form.reset();
+  }
 };
 
-window.hello = function() {
-  html = JST['app/templates/hello.us']({text: helloText()});
-  document.body.innerHTML += html;
+var storageProviders = {
+  session: sessionStorage,
+  local: localStorage
 };
 
-if(window.addEventListener) {
-  window.addEventListener('DOMContentLoaded', hello, false);
-} else {
-  window.attachEvent('onload', hello);
-}
+var onSubmit = function(event) {
+  event.preventDefault();
+  handle[document.activeElement.value](storageProviders[this.id], this);
+};
+
+document.getElementById('session').addEventListener('submit', onSubmit);
+document.getElementById('local').addEventListener('submit', onSubmit);
