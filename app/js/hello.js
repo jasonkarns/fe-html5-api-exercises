4f01cd77cc5287557c408805bddfdc5d558c584b
Jason Karns <jason@karns.name>
2015-06-21 00:13:46 -0400

    implementation

--- app/js/hello.js
+++ app/js/hello.js
@@ -2,3 +2,8 @@ function mapUrlFor(lat, lon) {
   return JST['app/templates/map-url.us']({lat:lat, lon:lon});
 }
 
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position){
+    document.getElementById('map').src = mapUrlFor(position.coords.latitude, position.coords.longitude);
+  }, function(message) { console.log(message); });
+}
